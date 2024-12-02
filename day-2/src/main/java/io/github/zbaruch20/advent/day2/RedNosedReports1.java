86ea928ae67ed9d4e834a75f4fd49080a6bf5f8e
zbaruch20 <zbaruch20@gmail.com>
2024-12-02 09:48:41 -0500

    refactor: move safe report to common

--- day-2/src/main/java/io/github/zbaruch20/advent/day2/RedNosedReports1.java
+++ day-2/src/main/java/io/github/zbaruch20/advent/day2/RedNosedReports1.java
@@ -4,14 +4,10 @@ import io.github.zbaruch20.advent.common.PuzzleInputReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-
 public class RedNosedReports1 {
     private RedNosedReports1() {}
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RedNosedReports1.class);
-    public static final int MIN_DISTANCE = 1;
-    public static final int MAX_DISTANCE = 3;
 
     public static void main(String[] args) {
         LOGGER.info("Number of safe reports: {}", numSafeReports(RedNosedReportsCommon.DAY_2_INPUT_RESOURCE));
@@ -20,33 +16,7 @@ public class RedNosedReports1 {
     public static long numSafeReports(String resource) {
         return PuzzleInputReader.readLines(resource)
                 .stream()
-                .filter(RedNosedReports1::isSafe)
+                .filter(RedNosedReportsCommon::isSafe)
                 .count();
-    }
-
-    private static boolean isSafe(String report) {
-        List<Integer> levels = RedNosedReportsCommon.getLevels(report);
-
-        // initial checks to see if we're increasing or decreasing
-        if (levels.size() < 2) return false;
-        if (levels.get(0).equals(levels.get(1))) return false; // equal consecutive levels is unsafe
-        boolean isIncreasing = levels.get(1) > levels.get(0);
-
-        for (int i = 0; i < levels.size() - 1; i++) {
-            int curr = levels.get(i);
-            int next = levels.get(i + 1);
-            if (next > curr != isIncreasing) {
-                // if increasing/decreasing doesn't match initial, report is unsafe
-                return false;
-            }
-
-            int diff = Math.abs(next - curr);
-            if (diff < MIN_DISTANCE || diff > MAX_DISTANCE) {
-                // if diff is out of acceptable range, report is unsafe
-                return false;
-            }
-        }
-
-        return true;
     }
 }
--- day-2/src/main/java/io/github/zbaruch20/advent/day2/RedNosedReportsCommon.java
+++ day-2/src/main/java/io/github/zbaruch20/advent/day2/RedNosedReportsCommon.java
@@ -10,6 +10,35 @@ public class RedNosedReportsCommon {
     public static final String DAY_2_INPUT_RESOURCE = "day2_input.txt";
     public static final String DAY_2_TEST_RESOURCE = "day2_test.txt";
 
+    public static final int MIN_DISTANCE = 1;
+    public static final int MAX_DISTANCE = 3;
+
+    static boolean isSafe(String report) {
+        List<Integer> levels = getLevels(report);
+
+        // initial checks to see if we're increasing or decreasing
+        if (levels.size() < 2) return false;
+        if (levels.get(0).equals(levels.get(1))) return false; // equal consecutive levels is unsafe
+        boolean isIncreasing = levels.get(1) > levels.get(0);
+
+        for (int i = 0; i < levels.size() - 1; i++) {
+            int curr = levels.get(i);
+            int next = levels.get(i + 1);
+            if (next > curr != isIncreasing) {
+                // if increasing/decreasing doesn't match initial, report is unsafe
+                return false;
+            }
+
+            int diff = Math.abs(next - curr);
+            if (diff < MIN_DISTANCE || diff > MAX_DISTANCE) {
+                // if diff is out of acceptable range, report is unsafe
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     static List<Integer> getLevels(String report) {
         return Arrays.stream(report.split("\\s+"))
                 .map(Integer::parseInt)
